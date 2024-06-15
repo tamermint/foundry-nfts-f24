db60a87157edb8775290dfe96867cd7cdcf0bcd9
Vivek <vivek.mitra@outlook.com>
2024-06-15 18:31:01 +1000

    metadata updated for image and basic deploy script written

---
+++ script/DeployBasicNft.s.sol
@@ -0,0 +1,18 @@
+//DeployScript basics:
+//1. For foundry import script and inherit it
+//2. have run function return your src contract
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract DeployBasicNft is Script {
+    function run() external returns (BasicNft) {
+        vm.startBroadcast();
+        BasicNft basicNft = new BasicNft();
+        vm.stopBroadcast();
+        return basicNft;
+    }
+}
--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,15 +4,24 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    uint256 private s_tokenCounter;
+    uint256 private s_tokenCounter; //keep a counter of tokens
+    mapping(uint256 => string) private s_tokenIdtoUri; //to map tokenids to uri
 
-    constructor() ERC721("Woofie", "WOOF") {
+    constructor() ERC721("Woofie", "WFIE") {
+        //pass in the name of collection and the symbol
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdtoUri[s_tokenCounter] = tokenUri; //at key 0, put in tokenUri
+        _safeMint(msg.sender, s_tokenCounter); //mint token to sender
+        s_tokenCounter++; //increse key value for the next token
+    }
 
     function tokenURI(
         uint256 tokenID
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        //return the token URI
+        return s_tokenIdtoUri[tokenID];
+    }
 }
