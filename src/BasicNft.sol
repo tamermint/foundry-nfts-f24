2ec75fc13425f5680a3269f96d21dd7a380111dd
Vivek <vivek.mitra@outlook.com>
2024-06-15 16:31:21 +1000

    images created via DALLE and added

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNft {}
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Woofie", "WOOF") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {}
+
+    function tokenURI(
+        uint256 tokenID
+    ) public view override returns (string memory) {}
+}
